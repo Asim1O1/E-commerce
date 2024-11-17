582a05b5755919a29e5df5461c8c560950623b95
Asim Khadka <asimkhadka52@gmail.com>
2024-11-17 23:53:23 +0545

    optimized

--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ const port = _config.port || 5000;
 
 const startServer = async () => {
   try {
-    await connectToDB();
     app.listen(port, () => {
       console.log(`Server running on port ${port}...`);
     });
+    await connectToDB();
   } catch (error) {
     console.error("Error starting the server:", error);
     process.exit(1);
